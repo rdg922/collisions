6abca3a5fd77e6fd575fe76cdd564a5ae3462049
Rohit Dasgupta <rdg922@gmail.com>
2022-01-16 12:28:26 -0800

    Setup Project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Project from "./Project"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Project/>
+    </div>
+  );
+}
+
+export default App;
